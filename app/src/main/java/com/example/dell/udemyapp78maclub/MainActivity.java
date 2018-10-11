5004d99886218c5c164effac111698ad3033d2eb
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2018-10-11 06:18:26 +0530

    intent to secondActivity

--- app/src/main/java/com/example/dell/udemyapp78maclub/MainActivity.java
+++ app/src/main/java/com/example/dell/udemyapp78maclub/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.dell.udemyapp78maclub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.add_martial_art:
+                Intent addMartialArtIntent  = new Intent(MainActivity.this,AddMartialArtActivity.class);
+                startActivity(addMartialArtIntent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
