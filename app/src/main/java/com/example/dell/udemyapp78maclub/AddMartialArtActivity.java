79f80a2db38936abe403969d34cd0111498178c5
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2018-10-12 08:28:20 +0530

    AddMartialArt Activity layout and java file references

--- app/src/main/java/com/example/dell/udemyapp78maclub/AddMartialArtActivity.java
+++ app/src/main/java/com/example/dell/udemyapp78maclub/AddMartialArtActivity.java
@@ -1,16 +1,23 @@
 package com.example.dell.udemyapp78maclub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddMartialArtActivity extends AppCompatActivity {
-
+       EditText edtName,edtPrice,edtColor;
+       Button btnAddMarshalArt,btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_martial_art);
-        Toast.makeText(AddMartialArtActivity.this,"This is add martial art activity",Toast.LENGTH_SHORT).show();
+       edtName = findViewById(R.id.editTextName);
+       edtPrice = findViewById(R.id.editTextPrice);
+       edtColor  = findViewById(R.id.editTextColor);
+       btnAddMarshalArt = findViewById(R.id.btnAddMarshalArt);
+       btnBack = findViewById(R.id.button_back);
 
     }
+
 }
